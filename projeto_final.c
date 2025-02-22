295fa9b8ed05d07d0d445b275ab40b29250b349e
Astorolus-11 <alvesgustavao@gmail.com>
2025-02-22 16:44:13 -0300

    Terceira parte

--- projeto_final.c
+++ projeto_final.c
@@ -3,6 +3,14 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/adc.h"
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include <stdlib.h>
+#include <math.h>
+#include "matriz_leds.pio.h"
+#include "hardware/i2c.h"
+#include "ssd1306.h"
+#include "fonte.h"
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 //Pinos://----------------------------------------------------------------------------------------------------------------------------------
@@ -10,6 +18,7 @@ const uint led_verde = 11, led_vermelho = 13; //LEDs do RGB
 const uint botao_a = 5, botao_b = 6, botao_joy = 22; //Botões
 const uint buzzer_a = 10, buzzer_b = 21; //Buzzers
 const uint joy_x = 27, joy_y = 26; //Joystick
+const uint pin_matriz = 7; //Matriz de LEDs 5x5
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 //Protótipos das funções:-------------------------------------------------------------------------------------------------------------------
@@ -19,7 +28,9 @@ void setup_botoes(); //Configura os botões
 void bomba(); //Bomba d'água para encher o reservatório automaticamente
 void adc_setup(); //Configura o joystick
 void atualizar_reservatorio(uint16_t x, uint16_t y); //Atualiza o reservatório com o joystick, Eixo x: Vazão d'água. Eixo y: Direção do motor (Se vai encher ou esvaziar)
-void estado_reservatorio_led_rgb();
+void estado_reservatorio_led_rgb(); //Mostra o estado do reservatório nos LEDs RGB
+void estado_reservatorio_matriz(); //Mostra o estado do reservatório na matriz de LEDs 5x5
+uint32_t intensidade(double g, double r, double b); //Configura a intensidade do brilho dos LEDs da matriz
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 //Variáveis globais://----------------------------------------------------------------------------------------------------------------------
@@ -31,14 +42,62 @@ const uint16_t nivel_max_reservatorio = 10000, nivel_min_reservatorio = 0, nivel
 static int16_t reservatorio = 0;
 static int16_t incremento = 1000, incremento_2 = 500; 
 static bool estado_motor = false;
+const uint pixels = 25;
 //------------------------------------------------------------------------------------------------------------------------------------------
+
+//PIO:--------------------------------------------------------------------------------------------------------------------------------------
+static PIO pio;
+static uint sm;
+static uint offset;
+static uint32_t valor_led;
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+double estados_reservatorio [5][25] = {
+
+       {0.1, 0.1, 0.1, 0.1, 0.1, //0% 
+        0.1, 0.1, 0.1, 0.1, 0.1,
+        0.1, 0.1, 0.1, 0.1, 0.1,
+        0.1, 0.1, 0.1, 0.1, 0.1,
+        0.1, 0.1, 0.1, 0.1, 0.1},
+        
+        {0.3, 0.1, 0.1, 0.1, 0.3, //25% 0.3 branco, 0.1 vermelho, 0.2 verde
+         0.3, 0.1, 0.1, 0.1, 0.3,
+         0.3, 0.1, 0.1, 0.1, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.3, 0.3, 0.3, 0.3},
+        
+        {0.3, 0.1, 0.1, 0.1, 0.3, //50% 0.3 branco, 0.1 vermelho, 0.2 verde
+         0.3, 0.1, 0.1, 0.1, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.3, 0.3, 0.3, 0.3},
+        
+        {0.3, 0.1, 0.1, 0.1, 0.3, //75% 0.3 branco, 0.1 vermelho, 0.2 verde
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.3, 0.3, 0.3, 0.3},
+        
+        {0.3, 0.2, 0.2, 0.2, 0.3, //100% 0.3 branco, 0.2 verde
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.2, 0.2, 0.2, 0.3,
+         0.3, 0.3, 0.3, 0.3, 0.3}
+
+
+};
 
 int main()
 {   pwm_setup();
     setup_botoes();
     adc_setup();
     stdio_init_all();
-
+    //Configuração da PIO:----------------------------------------------------------------------------------------------------------------
+    pio = pio0; 
+    offset = pio_add_program(pio,&matriz_leds_program); //Adiciona o programa ao PIO
+    sm = pio_claim_unused_sm(pio,true); //Utiliza uma maquina de estado disponível
+    matriz_leds_program_init(pio, sm, offset, pin_matriz);
+    //------------------------------------------------------------------------------------------------------------------------------------
     uint64_t tempo_atual_main,last_time_main=0;
 
     //Interrupções:-----------------------------------------------------------------------------------------------------------------------
@@ -84,12 +143,15 @@ int main()
                 uint16_t valor_y = adc_read();
 
                 atualizar_reservatorio(valor_x,valor_y);
+                
             
             }
             
 
         }
+        printf("Reservatório = %d\n",reservatorio);
         estado_reservatorio_led_rgb();
+        estado_reservatorio_matriz();
         
 
         sleep_ms(100);
@@ -285,6 +347,124 @@ void estado_reservatorio_led_rgb(){
 
     pwm_set_gpio_level(led_vermelho, red_pwm);
     pwm_set_gpio_level(led_verde, green_pwm);
-    
-    
+
     }
+
+void estado_reservatorio_matriz(){
+
+    uint32_t valor_led;
+
+    if(reservatorio==nivel_min_reservatorio){ //0%
+
+        for(uint i=0; i<pixels; i++){
+
+            if(i>=0 && i<5 || i==5 || i==9 || i==10 || i==14 || i==15 || i==19 || i==20 || i==24){ //Cor branca do reservatório
+                valor_led = intensidade(estados_reservatorio[0][i],estados_reservatorio[0][i],estados_reservatorio[0][i]);
+                
+            }
+            else{
+                valor_led = intensidade(0.0,estados_reservatorio[0][i],0.0);//Vermelho
+
+            }
+            pio_sm_put_blocking(pio,sm,valor_led);
+            
+        }
+
+
+    }
+
+    else if(reservatorio>=nivel_max_reservatorio/4 && reservatorio<4900){// Entre 25% e 49%
+
+        for(uint i=0; i<pixels; i++){
+
+            if(i>=0 && i<5 || i==5 || i==9 || i==10 || i==14 || i==15 || i==19 || i==20 || i==24){ //Cor branca do reservatório
+                valor_led = intensidade(estados_reservatorio[0][i],estados_reservatorio[0][i],estados_reservatorio[0][i]);
+                
+            }
+            else if(i>5 && i<10){
+                valor_led = intensidade(estados_reservatorio[1][i],0.0,0.0); //Verde
+
+            }
+            else{
+                valor_led = intensidade(0.0,estados_reservatorio[1][i],0.0); //Vermelho
+
+                
+            }
+            pio_sm_put_blocking(pio,sm,valor_led);
+
+            
+
+        }
+    }
+    else if(reservatorio>=(nivel_max_reservatorio/2) && reservatorio<7400){//Ente 50% e 74%
+
+        for(uint i=0; i<pixels; i++){
+
+            if(i>=0 && i<5 || i==5 || i==9 || i==10 || i==14 || i==15 || i==19 || i==20 || i==24){ //Cor branca do reservatório
+                valor_led = intensidade(estados_reservatorio[0][i],estados_reservatorio[0][i],estados_reservatorio[0][i]);
+                
+            }
+        
+            else if(i>5 && i<10 || i>10 && i<14){
+            valor_led = intensidade(estados_reservatorio[1][i],0.0,0.0); //Verde
+
+            }
+            else{
+                valor_led = intensidade(0.0,estados_reservatorio[1][i],0.0); //Vermelho
+
+            }
+            pio_sm_put_blocking(pio,sm,valor_led); 
+        }
+
+    }
+    else if(reservatorio>=7500 && reservatorio<9999){ //75% a 99%
+
+        for(uint i=0; i<pixels; i++){
+
+            if(i>=0 && i<5 || i==5 || i==9 || i==10 || i==14 || i==15 || i==19 || i==20 || i==24){ //Cor branca do reservatório
+                valor_led = intensidade(estados_reservatorio[0][i],estados_reservatorio[0][i],estados_reservatorio[0][i]);
+                
+            }
+            else if(i>5 && i<10 || i>10 && i<14 || i>15 && i<19){
+                valor_led = intensidade(estados_reservatorio[1][i],0.0,0.0); //Verde
+
+            }
+            else{
+                valor_led = intensidade(0.0,estados_reservatorio[1][i],0.0); //Vermelho
+
+            }
+            pio_sm_put_blocking(pio,sm,valor_led);
+            
+        }
+
+    }
+    else if(reservatorio==nivel_max_reservatorio){
+
+        for(uint i=0; i<pixels; i++){
+           if(i>=0 && i<5 || i==5 || i==9 || i==10 || i==14 || i==15 || i==19 || i==20 || i==24){ //Cor branca do reservatório
+                valor_led = intensidade(estados_reservatorio[0][i],estados_reservatorio[0][i],estados_reservatorio[0][i]);
+                
+            }
+            else{
+                valor_led = intensidade(estados_reservatorio[1][i],0.0,0.0); //Verde
+
+            }
+            pio_sm_put_blocking(pio, sm, valor_led);
+
+        }
+       
+
+    }
+    
+    
+}
+
+uint32_t intensidade(double g, double r, double b){
+    unsigned char G, R, B; //Recebe um valor inteiro entre 0 e 255 (8 bits)
+
+    G = g*255;
+    R = r*255;
+    B = b*255;
+
+    return (G<<24) | (R<<16) | (B<<8); //A cada 8 bits é uma cor de 32 bits, os primeiros 8 bits não contam
+}
